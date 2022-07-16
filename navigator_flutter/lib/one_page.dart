2285d70774b61bde0d97e2a4df06c24ae9cbf296
LuaDlc <luadiaslc@gmail.com>
2022-07-16 09:47:15 -0300

    Trabalhando com passagem de parametros com Navigator e args

--- navigator_flutter/lib/one_page.dart
+++ navigator_flutter/lib/one_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigator_flutter/two_page.dart';
 
 class OnePage extends StatelessWidget {
   const OnePage({Key ? key}) : super(key: key);
@@ -8,11 +9,32 @@ class OnePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const TwoPage(args: 'teste')),
+            );
+          },
           child: Text('Ir para segunda pagina'),
         ),
 
       ),
     );
    }
+/*
+   passando parametros via MaterialPageRoute
+  -com um afinal string args:
+   *final String args;*
+  -Na const da class, todo objeto que nao é anulável e estive como parametro nomeado, te que
+  ser requerido
+  const TwoPage({Key ? key, *required this.args*}) : super(key: key);
+   NA PAGE TWO:
+   -Adicione o args na const do MaterialPageRoute
+   onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const TwoPage(*args: 'teste'*)),
+            );
+          },
+          child: Text('Ir para segunda pagina'),
+        ),
+   */
 }
